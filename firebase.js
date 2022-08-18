cdb926226c0fbe7b6e46a3d86e1ee13bdc5c6da2
ajulkjose246 <mail.ajulkjose@gmail.com>
2022-08-18 12:03:52 +0530

    change realtime to firestore database

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.9.0/firebase-database.js";
-
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+// import { getFirestore,collection, getDocs } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js"; 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3TKMBpNk0afNfDbJs3TJzs0hwu7KgBkA",
+  piKey: "AIzaSyA3TKMBpNk0afNfDbJs3TJzs0hwu7KgBkA",
   authDomain: "akj-movie-stream.firebaseapp.com",
   databaseURL: "https://akj-movie-stream-default-rtdb.firebaseio.com",
   projectId: "akj-movie-stream",
@@ -12,31 +13,61 @@ const firebaseConfig = {
   appId: "1:775429916532:web:1325237c87b1a982c14e21",
   measurementId: "G-NJ1R2MPXCZ"
 };
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
+
 $(document).ready(function () {
-  // poster images
-  for (let i = 1; i < 10; i++) {
-    onValue(ref(db, 'bolt_stream/' + "movies/" + 'mov' + i), (snapshot) => {
-      $("#mov-img" + i).attr("src", snapshot.val().ImageUrl);
-      $("#image-1").attr("src", snapshot.val().ImageUrl);
 
-      // $("#movie_name").text(snapshot.val().Name);
-      // $("#movie_Country").text(snapshot.val().Country);
-      // $("#movie_Genre").text(snapshot.val().Genre);
-      // $("#movie_Release").text(snapshot.val().Release);
-      // $("#movie_Director").text(snapshot.val().Director);
-      // $("#movie_Production").text(snapshot.val().Production);
-      // $("#movie_trailer").attr("href", snapshot.val().Trailer);
+  initializeApp(firebaseConfig);
 
-      updateStarCount(postElement, data);
-    });
+  const db = getFirestore()
+  const colRef = collection(db, 'movies')
+
+  getDocs(colRef)
+    .then((snapshot) => {
+      let movies = []
+      snapshot.docs.forEach((doc) => {
+        // movies.push({ ...doc.data(),id: doc.id})
+        displaydata(doc)
+      })
+      console.log(movies)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  function displaydata(doc) {
+    // alert(doc.data().name)
+    var ele = document.getElementById("row");
+    var mov_list = document.createElement('div');
+    ele.appendChild(mov_list);
+    mov_list.classList.add('col-6', 'col-sm-6', 'col-md-4', 'col-lg-2');
+    i = i + 1;
+    mov_list.id = 'columns' + i;
+
+    var ele = document.getElementById("columns" + i);
+    var mov_list = document.createElement('div');
+    ele.appendChild(mov_list);
+    mov_list.classList.add('card-img');
+    j = j + 1;
+    mov_list.id = 'card-img' + j;
+
+    var ele = document.getElementById("card-img" + j);
+    var mov_list = document.createElement('a');
+    ele.appendChild(mov_list);
+    mov_list.href = "./stream.html";
+    k = k + 1;
+    mov_list.id = 'mov-link' + k;
+
+    var ele = document.getElementById("mov-link" + k);
+    var mov_list = document.createElement('img');
+    ele.appendChild(mov_list);
+    mov_list.src = doc.data().ImageUrl;
+    mov_list.id = doc.id;
+    $("#image-1").attr("src", "htt");
+    
   }
-  $("#mov-link" + 1).click(function () {
-      onValue(ref(db, 'bolt_stream/' + "movies/" + 'mov' + 1), (snapshot) => {
-        $("#image-1").attr("src", snapshot.val().ImageUrl);
-        updateStarCount(postElement, data);
-      });
-  })
 
 })
--- script.js
+++ script.js
@@ -4,32 +4,5 @@ $(document).ready(function () {
         $('.loading-sec').hide();
         $('.navbar-sec').show();
     });
-    for (i = 1; i < 11; i++) {
-        //create element in html
-        var ele = document.getElementById("row");
-        var mov_list = document.createElement('div');
-        ele.appendChild(mov_list);
-        mov_list.classList.add('col-6', 'col-sm-6', 'col-md-4', 'col-lg-2');
-        mov_list.id = 'columns' + i;
-
-        var ele = document.getElementById("columns" + i);
-        var mov_list = document.createElement('div');
-        ele.appendChild(mov_list);
-        mov_list.classList.add('card-img');
-        mov_list.id = 'card-img'+i;
-
-        var ele = document.getElementById("card-img"+i);
-        var mov_list = document.createElement('a');
-        ele.appendChild(mov_list);
-        mov_list.href = "./stream.html";
-        mov_list.id = 'mov-link'+i;
-
-        var ele = document.getElementById("mov-link"+i);
-        var mov_list = document.createElement('img');
-        ele.appendChild(mov_list);
-        mov_list.src = "./img/card-img.jpg";
-        mov_list.id = 'mov-img'+i;
-
-
-    }
+   
 })

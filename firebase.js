2b2cf152aecf06e9dc8523e73fd939e630588a2e
ajulkjose246 <mail.ajulkjose@gmail.com>
2022-08-18 19:41:41 +0530

    Added some changes

--- firebase.js
+++ firebase.js
@@ -13,61 +13,22 @@ const firebaseConfig = {
   appId: "1:775429916532:web:1325237c87b1a982c14e21",
   measurementId: "G-NJ1R2MPXCZ"
 };
-
+// let getSelectedMovie = ()=>{
+//   return localStorage.getItem(mid) || 0;
+// }
 $(document).ready(function () {
-
   initializeApp(firebaseConfig);
-
   const db = getFirestore()
-  const colRef = collection(db, 'movies')
-
-  getDocs(colRef)
-    .then((snapshot) => {
-      let movies = []
+  const colRef = collection(db, 'movies/')
+  getDocs(colRef).then((snapshot) => {
       snapshot.docs.forEach((doc) => {
-        // movies.push({ ...doc.data(),id: doc.id})
         displaydata(doc)
       })
-      console.log(movies)
-    })
-    .catch(err => {
+    }).catch(err => {
       console.log(err.message)
     })
 
-  let i = 0;
-  let j = 0;
-  let k = 0;
-
   function displaydata(doc) {
-    // alert(doc.data().name)
-    var ele = document.getElementById("row");
-    var mov_list = document.createElement('div');
-    ele.appendChild(mov_list);
-    mov_list.classList.add('col-6', 'col-sm-6', 'col-md-4', 'col-lg-2');
-    i = i + 1;
-    mov_list.id = 'columns' + i;
-
-    var ele = document.getElementById("columns" + i);
-    var mov_list = document.createElement('div');
-    ele.appendChild(mov_list);
-    mov_list.classList.add('card-img');
-    j = j + 1;
-    mov_list.id = 'card-img' + j;
-
-    var ele = document.getElementById("card-img" + j);
-    var mov_list = document.createElement('a');
-    ele.appendChild(mov_list);
-    mov_list.href = "./stream.html";
-    k = k + 1;
-    mov_list.id = 'mov-link' + k;
-
-    var ele = document.getElementById("mov-link" + k);
-    var mov_list = document.createElement('img');
-    ele.appendChild(mov_list);
-    mov_list.src = doc.data().ImageUrl;
-    mov_list.id = doc.id;
-    $("#image-1").attr("src", "htt");
-    
+    $('#row').append("<div class='col-6 col-sm-6 col-md-4 col-lg-2 selmovie' movie='"+doc.id+"'><div class='card-img' id='card-img1'><a ><img src='"+doc.data().ImageUrl+"'></a></div></div>");
   }
-
 })
--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     $(window).on('load', function () {
         $('.loading-sec').hide();
         $('.navbar-sec').show();
-    });
-   
+    }); 
+
+    $(document).on("click",".selmovie",function(){
+        let selmovie = $(this).attr('movie');
+        localStorage.setItem("selected_movie",selmovie);
+        window.location.href="./stream.html";
+    })
 })
